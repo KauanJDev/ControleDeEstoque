d83f4b1e4debb0c3bfe31c9c9f543ef7089b0b0b
KauanJDev <152095276+KauanJDev@users.noreply.github.com>
2025-09-04 19:06:17 -0300

    Update Program.cs

--- ControleDeEstoque/Program.cs
+++ ControleDeEstoque/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -28,11 +27,10 @@ builder.Services.AddSession(o =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
